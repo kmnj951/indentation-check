4672f778fa6e5180003d703c8f3ddf356780b1c8
snowcrab <48473962+kmnj951@users.noreply.github.com>
2019-11-07 02:40:59 +0900

    initial commit

---
+++ AStyle_3.1_windows/AStyle/as-mingw-exe/CMakeFiles/astyle.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "AStyle.exe"
+  "AStyle.exe.manifest"
+  "AStyle.pdb"
+  "CMakeFiles/astyle.dir/src/ASBeautifier.cpp.obj"
+  "CMakeFiles/astyle.dir/src/ASEnhancer.cpp.obj"
+  "CMakeFiles/astyle.dir/src/ASFormatter.cpp.obj"
+  "CMakeFiles/astyle.dir/src/ASLocalizer.cpp.obj"
+  "CMakeFiles/astyle.dir/src/ASResource.cpp.obj"
+  "CMakeFiles/astyle.dir/src/astyle_main.cpp.obj"
+  "libAStyle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/astyle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
